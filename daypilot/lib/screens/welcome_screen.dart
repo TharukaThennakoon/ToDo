cd5f9d9d9365d35f07c93aaf22fc0951f6e62454
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2026-04-25 15:56:19 +0530

    add WelcomeScreen widget to welcome_screen.dart

--- daypilot/lib/screens/welcome_screen.dart
+++ daypilot/lib/screens/welcome_screen.dart
@@ -17,3 +17,15 @@ class DayPilotApp extends StatelessWidget {
   }
 }
 
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Welcome"),
+      ),
+    );
+  }
+}
