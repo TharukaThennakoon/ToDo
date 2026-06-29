523f0cbd39ff649b8ceee5bcffcc75fecd31b597
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2026-06-29 19:17:51 +0530

    feat: update Gradle configuration, add Firebase options, and include Google logo asset

---
+++ daypilot/lib/firebase_options.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        throw UnsupportedError('This platform is not supported');
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',           // ← from Firebase Console
+    appId: 'YOUR_APP_ID',             // ← from Firebase Console
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+  );
+}
